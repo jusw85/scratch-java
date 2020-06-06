2ff38762d7e63e5715afdcc90b9551f15d2a0361
Justin Wong <jusw85@hotmail.com>
2020-06-06 17:48:28 +0800

    Cleanup scratch

--- src/main/java/Scratch.java
+++ src/main/java/Scratch.java
@@ -1,12 +1,6 @@
 public class Scratch {
 
     public static void main(String[] args) {
-//		for (int i = 2; i <= 52; i++) {
-//			System.out.println(
-//					"UNION (SELECT t1.* FROM _m_vessel_positions t2 INNER JOIN vessel_positions_y2016w"
-//							+ i + " t1 ON t1.pk = t2.pk)");
-//		}
-
     }
 
 }
